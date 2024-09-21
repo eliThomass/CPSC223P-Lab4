46dab3c4754caeb587f046267b85ecccd5c16b87
eliThomass <ecthomas05@gmail.com>
2024-09-21 10:56:56 -0700

    fixing bugs!

--- contacts.py
+++ contacts.py
@@ -9,40 +9,41 @@ def print_list():
 	print("=================== CONTACT LIST ======================")
 	print("Last Name             First Name	     Phone")
 	print("====================  ====================   ==========")
-	for ID in contact_dict_:
-		print("{:<21} {:<22} {:<22}".format(contact_dict_[ID][1], contact_dict_[ID][0], ID))
+	for id in contact_dict_:
+		print("{:<21} {:<22} {:<22}".format(contact_dict_[id][1], contact_dict_[id][0], id))
 
-def add_contact(contact_dict, ID_ = 0, first_name = "N/A", last_name = "N/A"):
+def add_contact(contact_dict, id = 0, first_name = "N/A", last_name = "N/A"):
 	"""adds a contact to end of list"""
 	try:
-		if ID_ in contact_dict_: 
-			print("error.")
+		if id in contact_dict: 
+			return "error"
 		else: 
-			contact_dict[ID_] = [first_name, last_name]
+			contact_dict[id] = [first_name, last_name]
 			print("Added:", first_name, last_name)
-		return contact_dict[ID_]
+			return contact_dict[id]
 	except:
 		print("Invalid input.")
 
-def modify_contact(contact_dict, ID_ = 0, first_name = "N/A", last_name = "N/A"):
+def modify_contact(contact_dict, id = 0, first_name = "N/A", last_name = "N/A"):
 	"""modify a contact within a valid index range"""
 	try:
-		if ID_ in contact_dict_:
-			contact_dict[ID_] = [first_name, last_name]
+		if id in contact_dict:
+			contact_dict[id] = [first_name, last_name]
 			print("Modified: ", first_name, last_name)
+			return contact_dict[id]
 		else:
 			print("error.")
-		return contact_dict[ID_]
 	except:
 		print("Invalid input.")
 	
-def delete_contact(contact_dict, ID_ = 0):
+def delete_contact(contact_dict, id = 0):
 	"""delete contact within valid index range"""
 	try:
-		if ID_ in contact_dict:
-			contact_dict.pop(ID_)
+		if id in contact_dict:
+			return_val = [id[0], id[1]]
+			contact_dict.pop(id)
 		else:
-			print("error.")
+			return "error"
 	except:
 		print("Invalid input.")
 		
